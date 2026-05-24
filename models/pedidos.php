016fdf307b7a12c1992fa6b93b1ce51889ed3fb3
Sara Martinez <est.sara.vmartinez@unimilitar.edu.co>
2026-05-24 16:56:24 -0500

    correción models

--- models/pedidos.php
+++ models/pedidos.php
@@ -1,6 +1,5 @@
 <?php
-
-require_once __DIR__ . '/../database/conexion.php';
+require_once __DIR__ . '/../database/conection.php';
 
 // CREAR pedido
 function crearPedidoModel($usuario_id, $total) {
