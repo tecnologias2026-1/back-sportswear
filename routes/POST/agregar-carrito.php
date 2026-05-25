68863babc4efffa4eba84c1a02963f83e9773763
Sara Martinez <est.sara.vmartinez@unimilitar.edu.co>
2026-05-24 21:29:50 -0500

    correcion index

--- routes/POST/agregar-carrito.php
+++ routes/POST/agregar-carrito.php
@@ -1,15 +1,15 @@
-    <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    header("Content-Type: application/json; charset=UTF-8");
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=UTF-8");
 
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        http_response_code(200);
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+     http_response_code(200);
         exit();
-    }
+}
 
-    require_once __DIR__ . '/../../models/carrito.php';
+require_once __DIR__ . '/../../models/carrito.php';
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
