db9accfeb874f849e21f4bb2172247c8311d37ee
Sofia Terrios <sterrios1013@gmail.com>
2026-05-24 20:40:09 -0500

    fix cors headers

--- apis/index.php
+++ apis/index.php
@@ -1,7 +1,11 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json");
+
+// Responder preflight OPTIONS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
--- routes/GET/lista-productos.php
+++ routes/GET/lista-productos.php
@@ -1,8 +1,15 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
-header("Content-Type: application/json; charset=UTF-8");
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json");
+
+// Responder preflight OPTIONS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 require_once __DIR__ . '/../../models/productos.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
